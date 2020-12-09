fa292d7e94b36a2e1ac1c55653b8da0a0cb9e97d
Chiman Seyedhashemi <c.seyedhashemi@catod.ir>
2020-12-10 00:38:20 +0330

    create base route

---
+++ src/config/route/base.route.ts
@@ -0,0 +1,46 @@
+import { EROUT } from "./routes";
+
+interface IRouteBase {
+  name: string;
+  sidebarVisible: boolean;
+  icon?: string;
+}
+
+export interface IRoute extends IRouteBase {
+  path: string;
+}
+
+export type IAppRoute = Array<IRoute>;
+
+export class AppRoute {
+  private static readonly routes: IAppRoute = [
+    {
+      path: EROUT.USERS,
+      name: "users",
+      sidebarVisible: false,
+      icon: "fa fa-dashboard",
+    },
+    {
+      path: EROUT.PROFILE,
+      name: "profile",
+      sidebarVisible: true,
+      icon: "fa fa-picture-o",
+    },
+    {
+      path: EROUT.SEARCH,
+      name: "search",
+      sidebarVisible: true,
+      icon: "fa fa-search-o",
+    },
+    {
+      path: EROUT.TODO,
+      name: "todo",
+      icon: "fa fa-link",
+      sidebarVisible: true,
+    },
+  ];
+
+  static getRoutes(): IAppRoute {
+    return this.routes;
+  }
+}
