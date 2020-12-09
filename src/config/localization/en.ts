9ab35fad61759b6cf094bf4de5365151a9f9480d
Chiman Seyedhashemi <c.seyedhashemi@catod.ir>
2020-12-10 00:34:11 +0330

    add localization

--- src/config/localization/en.ts
+++ src/config/localization/en.ts
@@ -1,2 +1,11 @@
-export const en = { };
+export const en = {
+    ddt_test:"DDT Test",
+    login:"Login",
+    username:"username",
+    password:"password",
+    name:"name",
+    phone:"phone",
+    edit:"Edit",
+    delete:"Delete"
+ };
   
--- src/config/localization/localization.ts
+++ src/config/localization/localization.ts
@@ -2,6 +2,14 @@ import { en } from "./en";
 
 interface ILocalization {
   [key: string]: any;
+  ddt_test:string;
+  login:string;
+  username:string;
+  password:string;
+  name:string;
+  phone:string;
+  edit:string;
+  delete:string;
 }
 
 export const Localization: ILocalization = en;
