006a33e06e011076ef540751a2f3ec557d62f39e
Chiman Seyedhashemi <c.seyedhashemi@catod.ir>
2020-12-10 00:27:55 +0330

    add new route

--- src/component/app/App.tsx
+++ src/component/app/App.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { HashRouter, Redirect, Route, BrowserRouter as Router, Switch } from "react-router-dom"
-import { EROUT } from "../../config/routes";
+import { EROUT } from "../../config/route/routes";
 import { RouteLayoutValidUser } from "../layout/validUser/routeLayoutValidUser";
 import { RouteGeneral } from "../layout/general/routeGeneral";
 import { Login } from "../pages/login/login";
@@ -10,6 +10,9 @@ const appRoutes = (
     <Switch>
       <Route exact path={EROUT.MAIN} component={() => <Redirect to={EROUT.USERS} />} />
       <RouteLayoutValidUser exact path={EROUT.USERS} />
+      <RouteLayoutValidUser exact path={EROUT.SEARCH} />
+      <RouteLayoutValidUser exact path={EROUT.TODO} />
+      <RouteLayoutValidUser exact path={EROUT.PROFILE} />
       <RouteGeneral path={EROUT.LOGIN} component={Login} />
       <RouteLayoutValidUser />
     </Switch>
