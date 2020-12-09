7f57560e650ef030845d881e15f3e41db2a61c00
Chiman Seyedhashemi <c.seyedhashemi@catod.ir>
2020-12-10 00:30:06 +0330

    create main layout

--- src/component/layout/validUser/main.tsx
+++ src/component/layout/validUser/main.tsx
@@ -2,6 +2,12 @@ import { connect, MapDispatchToProps } from 'react-redux';
 import { Dispatch } from 'redux';
 import { BaseComponent } from '../../base/baseComponent';
 import { History } from "history";
+import { IReduxState } from '../../../redux/appState';
+import { GeneralHeader } from '../general/generalHeader';
+import React from 'react';
+import { GeneralFooter } from '../general/generalFooter';
+import { AppRoute } from '../../../config/route/base.route';
+import { Link } from 'react-router-dom';
 
 interface IProps {
     history: History;
@@ -15,7 +21,23 @@ class LayoutMainComponent extends BaseComponent<IProps, IState> {
     render() {
         return (
             <>
-                main
+                <div className="valid-layout-wrapper d-flex">
+                    <GeneralHeader />
+                    <div className="main-valid-layout">
+                        <aside className="sidebar">
+                            <ul>
+                                {AppRoute.getRoutes().map(route => {
+                                    return (<li className="mb-1 pl-3" key={route.name}><Link to={route.path}>{route.name}</Link></li>)
+                                })}
+                            </ul>
+                        </aside>
+                        <div className="m-2 w-100">
+                            {this.props.children}
+                        </div>
+
+                    </div>
+                    <GeneralFooter />
+                </div>
             </>
         )
     }
@@ -25,7 +47,7 @@ const dispatch2props: MapDispatchToProps<{}, {}> = (dispatch: Dispatch) => {
     return {}
 }
 
-const state2props = (state: any) => {
+const state2props = (state: IReduxState) => {
     return {}
 }
 
