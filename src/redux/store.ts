f1837902154ef787fb1045db767f75da372a1dac
Chiman Seyedhashemi <c.seyedhashemi@catod.ir>
2020-12-10 00:36:43 +0330

    update store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,12 +3,20 @@ import {
     createStore,
     ReducersMapObject,
     AnyAction,
+    Reducer,
   } from "redux";
+  import { reducer as UserReducer } from "./reducer/user";
+  import { reducer as TokenReducer } from "./reducer/token";
+  import { reducer as UsersReducer } from "./reducer/users";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import { IUser } from "../model/model.user";
+import { IToken } from "../model/model.token";
 
 const reducers: ReducersMapObject<any, AnyAction> = {
-
+  logged_in_user: UserReducer as Reducer<IUser | null, AnyAction>,
+  token: TokenReducer as Reducer<IToken|null, AnyAction>,
+  users: UsersReducer as Reducer<Array<IUser>, AnyAction>
   };
   
   export const main_reducer = combineReducers(reducers);
